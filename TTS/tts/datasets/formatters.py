c385452d6f2ba8515682ba3ea82ac98aabaa5df5
Muhammad-Abdelsattar <muhammadabdalsattar@gmail.com>
2025-01-06 21:11:05 +0000

    Added arabic formatters(the same as ljspeech ones).

--- TTS/tts/datasets/formatters.py
+++ TTS/tts/datasets/formatters.py
@@ -224,7 +224,7 @@ def arabic_single_speaker(root_path, meta_file, **kwargs):  # pylint: disable=un
     txt_file = os.path.join(root_path, meta_file)
     items = []
     speaker_name = "Arabic_single_speaker"
-    with open(txt_file, "r", encoding="utf-8") as ttf:
+    with open(txt_file, "r", encoding="utf-16") as ttf:
         for line in ttf:
             cols = line.split("|")
             wav_file = os.path.join(root_path, "wavs", cols[0] + ".wav")
