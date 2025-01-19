5330ffc9dcac470e7e275caaa44b4f4fe76853fc
Muhammad-Abdelsattar <muhammadabdalsattar@gmail.com>
2025-01-19 21:27:01 +0000

    Updated cleaners

--- TTS/tts/datasets/formatters.py
+++ TTS/tts/datasets/formatters.py
@@ -224,7 +224,7 @@ def arabic_single_speaker(root_path, meta_file, **kwargs):  # pylint: disable=un
     txt_file = os.path.join(root_path, meta_file)
     items = []
     speaker_name = "Arabic_single_speaker"
-    with open(txt_file, "r", encoding="utf-16") as ttf:
+    with open(txt_file, "r") as ttf:
         for line in ttf:
             cols = line.split("|")
             wav_file = os.path.join(root_path, "wavs", cols[0] + ".wav")
@@ -235,7 +235,7 @@ def arabic_single_speaker(root_path, meta_file, **kwargs):  # pylint: disable=un
 def arabic_single_speaker_test(root_path, meta_file, **kwargs):  # pylint: disable=unused-argument
     txt_file = os.path.join(root_path, meta_file)
     items = []
-    with open(txt_file, "r", encoding="utf-16") as ttf:
+    with open(txt_file, "r") as ttf:
         speaker_id = 0
         for idx, line in enumerate(ttf):
             # 2 samples per speaker to avoid eval split issues
